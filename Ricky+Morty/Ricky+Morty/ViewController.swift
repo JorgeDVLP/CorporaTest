fd450f7250f7e9f97feadadc9a62a8fcd7571ca6
JorgeDVLP <sotikDVLP@gmail.com>
2022-06-03 10:11:01 +0200

    Project created

---
+++ Ricky+Morty/Ricky+Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ricky+Morty
+//
+//  Created by Jorge Martin Moreno on 3/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
