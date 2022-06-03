f3efb77c92c11debea32ae7b48848ffcaa6938ae
JorgeDVLP <sotikDVLP@gmail.com>
2022-06-03 10:53:53 +0200

    Character model created

---
+++ Ricky+Morty/Ricky+Morty/Entities/Character.swift
@@ -0,0 +1,23 @@
+//
+//  Character.swift
+//  Ricky+Morty
+//
+//  Created by Jorge Martin Moreno on 3/6/22.
+//
+
+import Foundation
+
+enum CharacterStatus {
+    case alive
+    case death
+    case unknown
+}
+
+// una imagen circular del personaje, su nombre, su planeta de origen y su status actual (vivo, muerto o desconocido).
+struct Character {
+    let id: Int
+    let name: String
+    let origin: String
+    let status: CharacterStatus
+    let imageURL: String
+}
